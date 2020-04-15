3832f516684b759cf601c8a0c5d6d6b966dc1fa3
sofi.sher <sofi.sheromova@gmail.com>
2020-04-15 14:28:37 +0500

    application basis: head, navbar, profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <div className={style.profile}>
+      <div>description</div>
+      <div>photo</div>
+      <div>my secrets</div>
+      <div>add secrets</div>
+      <div>secrets</div>
+    </div>
+  );
+};
+
+export default Profile;
