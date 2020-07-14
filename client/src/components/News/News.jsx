d7e250fcb95ca6e12dcd9bb285e07fb44215ff0a
Sofi <sofi.sheromova@gmail.com>
2020-07-14 18:10:45 +0500

    class component for News (bug)

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -3,27 +3,37 @@ import * as axios from 'axios';
 import style from './News.module.css';
 import PropTypes from 'prop-types';
 
-const News = ({news, setNews}) => {
-  axios.get('/api/getList').then((response) => {
-    console.log(response);
-    if (news.length === 0 ) {
-      setNews(response.data);
-    }
-  });
-  return (
-    <div className={style.news}>
-      <h1>Your news</h1>
-      {
-        news.map((item) => {
-          return (<div key={item.id}>{item.content}</div>);
-        })
-      }
-    </div>
-  );
-};
+class News extends React.Component {
+  constructor(props) {
+    super(props);
+    this.news = props.news;
+    this.setNews = props.setNews;
+  }
+
+  componentDidMount() {
+    axios.get('/api/getList')
+        .then((response) => {
+          this.setNews(response.data);
+        });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className={style.news}>
+          {
+            this.news.map((item) => {
+              return <div key={item.id}>{item.content}</div>;
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 News.propTypes = {
-  news: PropTypes.object.isRequired,
+  news: PropTypes.array.isRequired,
   setNews: PropTypes.func.isRequired,
 };
 
